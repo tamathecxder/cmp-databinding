6a39ad09c2857914b4cc5dd9416c096280d1261e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-11 14:47:10 +0700

    custom event binding for emit data to outside of the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,20 @@ export class AppComponent {
     name: 'Testserver',
     content: 'Just a test!'
   }];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    const newLocal = this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
