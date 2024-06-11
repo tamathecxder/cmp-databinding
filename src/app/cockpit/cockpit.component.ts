7a637b06c19d0b96c58faf04fc8c04fe48cb2e61
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-11 14:11:56 +0700

    init cockpit component

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrl: './cockpit.component.css'
+})
+export class CockpitComponent {
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() {}
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
