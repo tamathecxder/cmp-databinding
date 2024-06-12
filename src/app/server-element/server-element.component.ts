df743b35cfa8ad1792b7f69865adb378f7cc23d5
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-12 11:08:33 +0700

    angular lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -11,4 +11,42 @@ export class ServerElementComponent {
     name: string,
     content: string,
   };
+
+  constructor() {
+    console.log('constructor executed');
+  }
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    console.log('ngOnChanges executed');
+
+    console.log(simpleChanges);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit executed');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck executed');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit executed');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked executed');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit executed');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked executed');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy executed');
+  }
 }
