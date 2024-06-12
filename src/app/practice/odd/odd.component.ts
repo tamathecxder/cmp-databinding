f99a997dee774d31c334936f7d192c6d66835882
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-12 12:48:09 +0700

    implement odd component

---
+++ src/app/practice/odd/odd.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-odd',
+  templateUrl: './odd.component.html',
+  styleUrl: './odd.component.css'
+})
+export class OddComponent {
+  @Input('odds') oddNumbers: number[];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
