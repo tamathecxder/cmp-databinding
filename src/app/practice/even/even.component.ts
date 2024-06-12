c0c87d3acf1ae9641f785ae0019c476c115a9225
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-12 12:48:00 +0700

    implement even component

---
+++ src/app/practice/even/even.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  templateUrl: './even.component.html',
+  styleUrl: './even.component.css'
+})
+export class EvenComponent {
+  @Input('evens') evenNumbers: number[];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
